fcf57d85a3ef8b56c7d8378d896e833b968dafd3
Aliaksei Kalymaha <alexkolymago@gmail.com>
2024-10-26 20:58:08 +0300

    add solutions

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -129,8 +129,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.top > rect2.top + rect2.height
+    || rect1.top + rect1.height < rect2.top
+    || rect1.left > rect2.left + rect2.width
+    || rect1.left + rect1.width < rect2.left) return false;
+  return true;
 }
 
 
@@ -160,8 +164,11 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const distance = Math.abs(Math.sqrt((circle.center.x - point.x)
+  ** 2) + Math.sqrt((circle.center.y - point.y) ** 2));
+  if (distance < circle.radius) return true;
+  return false;
 }
 
 
